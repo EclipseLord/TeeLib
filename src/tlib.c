4582155d081daeb5a4c510ba63034ae122fabd8b
Eclipse <aidenmonkeybutt@live.com>
2021-07-09 06:30:54 -0700

    return value (oops)

--- src/tlib.c
+++ src/tlib.c
@@ -3,9 +3,11 @@
 __declspec(dllexport) int TeeLib_Init()
 {
 	printf("Started TeeLib :)\n");
+	return 1;
 }
 
 __declspec(dllexport) int TeeLib_Destroy()
 {
 	printf("Good bye from TeeLib\n");
+	return 1;
 }
